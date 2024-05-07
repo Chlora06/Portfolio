3d8cff8d23d4a21e262ee88440dc4d4eb914ce80
Chlora06 <zliu14@mail.sfsu.edu>
2024-05-07 11:50:10 -0700

    add responsible navbar

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -1,35 +1,39 @@
-
+import {useState} from 'react';
 const Navbar = () => {
+  const[isOpen, setIsOpen] = useState(false);
+
   return (
     <nav className="nav-bar">
       <div className="nav-logo">Portfolio</div>
-
-      <ul className="menu-list">
-        <li className="menu-list-items">
+      <button className='hamburger' onClick={()=>setIsOpen(!isOpen)}>
+        &#9776;
+      </button>
+      <div className={`menu-list ${isOpen ? 'open': ''}`}>  
+        <div className="menu-list-items">
           <a href="#" className="menu-list-items-link">
             <div className="dot"></div>
             Home
           </a>
-        </li>
-        <li className="menu-list-items">
+        </div>
+        <div className="menu-list-items">
           <a href="#About" className="menu-list-items-link">
             <div className="dot"></div>
             About Me
           </a>
-        </li>
-        <li className="menu-list-items">
+        </div>
+        <div className="menu-list-items">
           <a href="#Projects" className="menu-list-items-link">
             <div className="dot"></div>
             Projects
           </a>
-        </li>
-        <li className="menu-list-items">
+        </div>
+        <div className="menu-list-items">
           <a href="#Contact" className="menu-list-items-link">
             <div className="dot"></div>
             Contact Me
           </a>
-        </li>
-      </ul>
+        </div>
+      </div>
     </nav>
   );
 }
